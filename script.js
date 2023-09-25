33c12e79714a2fbd9aaa74862c839e882cdf8aca
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-25 11:34:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your code here
+const divs = document.querySelectorAll(".image")
+
+divs.forEach(div=>{
+	div.addEventListener('dragstart',(e)=>{
+		e.dataTransfer.setData('id', e.target.id)
+	})
+})
+divs.forEach(div=>{
+	div.addEventListener('dragover',(e)=>{
+		e.preventDefault()
+	})
+	div.addEventListener('drop',(e)=>{
+		const drag = document.getElementById(e.dataTransfer.getData('id'))
+	    let tid = drag.id
+		drag.id = div.id
+		div.id = tid
+		let temp = div.innerText
+		div.innerText  = drag.innerText
+		drag.innertext = temp
+	})
+})
