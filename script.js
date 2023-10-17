bf1ac90d404cd8c07674f9805e77abce44a911b8
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-10-17 16:01:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const divs = document.querySelectorAll(".image")
-
-divs.forEach(div=>{
-	div.addEventListener('dragstart',(e)=>{
-		e.dataTransfer.setData('id', e.target.id)
-	})
-}) 
-divs.forEach(div=>{
-	div.addEventListener('dragover',(e)=>{
-		e.preventDefault()
-	})
+let dragindex = 0;
+let dropindex = 0;
+let clone = "";
+ 
+const images = document.querySelectorAll(".image");
+ 
+function drag(e) {
+  e.dataTransfer.setData("text", e.target.id);
+} 
+ 
+function allowDrop(e) {
+  e.preventDefault();
+}
+ 
+function drop(e) {
+  clone = e.target.cloneNode(true);
+  let data = e.dataTransfer.getData("text");
+  let nodelist = document.getElementById("parent").childNodes;
+  console.log(data, e.target.id);
+  for (let i = 0; i < nodelist.length; i++) {
+    if (nodelist[i].id == data) {
+      dragindex = i;
+    }
+  }
+ 
+  dragdrop(clone);
+ 
+  document
+    .getElementById("parent")
+    .replaceChild(document.getElementById(data), e.target);
+ 
+  document
+    .getElementById("parent")
+    .insertBefore(
+      clone,
+      document.getElementById("parent").childNodes[dragindex]
+    );
+}
+ 
+const dragdrop = (image) => {
+  image.ondragstart = drag;
+  image.ondragover = allowDrop;
+  image.ondrop = drop;
+};
+ 
+images.forEach(dragdrop);
 	div.addEventListener('drop',(e)=>{
 		const drag = document.getElementById(e.dataTransfer.getData('id'))
 	    let tid = drag.id
